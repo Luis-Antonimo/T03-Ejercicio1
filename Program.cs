965677d975ccab0cb7fc3d0059009deadb16fcaa
Luis Antónimo <luisantonimo@gmail.com>
2022-07-10 23:09:14 +0200

    Ok

--- Program.cs
+++ Program.cs
@@ -1,3 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
 
+namespace T03Ejercicio1
+{
+    class Program
+    {
+        static void Main()
+        {
+            int x = 10;
+            int y = 1000;
+            Console.WriteLine("La variable X= {0} La variable Y = {1}", x, y);
+            Console.WriteLine("Suma X+Y = {0}", x + y);
+            Console.WriteLine("Resta X-Y = {0}", x - y);
+            Console.WriteLine("Multiplicación X*Y = {0}", x * y);
+            Console.WriteLine("Division X/Y = {0}", x / y);
+            Console.WriteLine("Modulo X%Y = {0}", x % y);
+        }
+    }
+}
